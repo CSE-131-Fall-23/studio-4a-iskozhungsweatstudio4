ddb92549fcd77df5959b68c7d7a37b953be2ff83
emilyhung <emilyhung@localhost>
2023-10-18 11:08:32 -0500

    pushed

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,39 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
-	}
+	
+		
+		for (int i=0; i<100; i++)
+		{
+		
+		int A = (int) (Math.random()*255);
+		int B = (int) (Math.random()*255);
+		int C = (int) (Math.random()*255);
+		double D = (double)(Math.random());
+		double E = (double)(Math.random());
+		double F = (double)(Math.random());
+		double G = (double)(Math.random());
+	
+		
+		
+		
+		Color gross1 = new Color(A, B, C);
+		StdDraw.setPenColor(gross1);
+		if (i%2==0)
+		{StdDraw.filledRectangle(D, E, F, G);}
+		else
+		{StdDraw.filledEllipse(D, E, F, G);}
+		
+		Color white = new Color(255, 255, 255);
+		StdDraw.setPenColor(white);
+		StdDraw.filledRectangle(.5, 0, 1, .2);
+		StdDraw.filledRectangle(.5, 1, 1, .2);
+		StdDraw.filledRectangle(0, .5, .1, 1);
+		StdDraw.filledRectangle(1, .5, .1, 1);
+		
+	
+		
+		i--;
+		}
+}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,44 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+String shape = in.next();
+int colorR = in.nextInt();
+int colorG =in.nextInt();
+int colorB = in.nextInt();
+boolean filled = in.nextBoolean();
+
+if (shape == ("rectangle") || shape == "ellipse") 
+	{
+	double x = in.nextDouble();
+	double y = in.nextDouble();
+	double halfWidth = in.nextDouble();
+	double halfHeigh = in.nextDouble();
+		Color rectangle = new Color (colorR, colorG, colorB);
+		StdDraw.setPenColor(rectangle);
+		if (filled = true)
+			{StdDraw.filledRectangle(x,  y,  halfWidth,  halfHeigh);}
+		if (filled = false)
+			{StdDraw.rectangle(x,  y,  halfWidth,  halfHeigh);}
 	}
+if (shape == "triangle")
+{
+
+	double x1 = in.nextDouble();
+	double y1 = in.nextDouble();
+	double x2 = in.nextDouble();
+	double y2 = in.nextDouble();
+	double x3 = in.nextDouble();
+	double y3 = in.nextDouble();
+	double[] x = { x1, x2, x3 };
+	double[] y = { y1, y2, y3};
+	Color triangle = new Color (colorR, colorG, colorB);
+	StdDraw.setPenColor(triangle);
+	if (filled = true)
+		{StdDraw.filledPolygon(x,y);}
+	if (filled = false)
+		{StdDraw.polygon(x,y);}
+
+	}
+	
+}
 }
